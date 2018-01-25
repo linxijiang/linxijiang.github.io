3246534d4e9fe6bf9c58e1baf743f68cca290c7e
林锡江[DMP] <xijiang.lin@pjbest.com>
2018-01-25 10:59:36 +0800

    A

---
+++ common.js
@@ -0,0 +1,19 @@
+// var CONST_MONTH_DATA = [{name:'一月', value:1}, {name:'二月', value:2}, {name:'三月', value:3}, {name:'四月', value:4}, {name:'五月', value:5}, {name:'六月', value:6}, {name:'七月', value:7}, {name:'八月', value:8}, {name:'九月', value:9}, {name:'十月', value:10}, {name:'十一月', value:11}, {name:'十二月', value:12}];
+var CONST_TRAINING_ROOM = [{name:'401',value:'401'},{name:'402',value:'402'},{name:'403',value:'403'},{name:'404',value:'404'},{name:'405',value:'405'},{name:'406',value:'406'}];
+//区分是否可预订背景颜色
+var CAN_NOT_BOOK_COLOR = 'background-color:red;';
+var BOOKING_COLOR = 'background-color:#7fb80e;';
+var CAN_BOOK_COLOR = 'background-color:#afdfe4;';
+
+var CONST_BOOK_TIME_TYPE = [{name:'上午', value:0},{name:'下午', value:1},{name:'晚上', value:2}];
+
+var CONST_MORNING_TIME = [{name:'08:00',value:'08:00'},{name:'08:30',value:'08:30'},{name:'09:00',value:'09:00'},{name:'09:30',value:'09:30'},{name:'10:00',value:'10:00'},{name:'10:30',value:'10:30'},{name:'11:00',value:'11:00'},{name:'11:30',value:'11:30'},{name:'12:00',value:'12:00'}];
+var CONST_AFTERNOON_TIME = [{name:'12:00',value:'12:00'},{name:'12:30',value:'12:30'},{name:'13:00',value:'13:00'},{name:'13:30',value:'13:30'},{name:'14:00',value:'14:00'},{name:'14:30',value:'14:30'},{name:'15:00',value:'15:00'},{name:'15:30',value:'15:30'},{name:'16:00',value:'16:00'},{name:'16:30',value:'16:30'},{name:'17:00',value:'17:00'},{name:'17:30',value:'17:30'},{name:'18:00',value:'18:00'}];
+var CONST_NIGHT_TIME = [{name:'18:00',value:'18:00'},{name:'18:30',value:'18:30'},{name:'19:00',value:'19:00'},{name:'19:30',value:'19:30'},{name:'20:00',value:'20:00'},{name:'20:30',value:'20:30'},{name:'21:00',value:'21:00'},{name:'21:30',value:'21:30'},{name:'22:00',value:'22:00'}];
+
+var MONING_START_TIME = '08:00';
+var MONING_END_TIME = '12:00';
+var AFTERNOON_START_TIME = '12:00';
+var AFTERNOON_END_TIME = '18:00';
+var NIGHT_START_TIME = '18:00';
+var NIGHT_END_TIME = '22:00';
